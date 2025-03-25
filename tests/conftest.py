da9a8aeacf88ce813652d57e6a984bbad77195a0
Yasen Trahnov <ytrahnov@redhat.com>
2025-03-25 18:01:28 +0100

    missing tests

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import typing as t
+from urllib.parse import urljoin
+import os
+
+import pytest
+
+pytest_plugins = "pytest_pulp_cli"
+
+
+@pytest.fixture
+def pulp_cli_vars(pulp_cli_vars: t.MutableMapping[str, str]) -> t.MutableMapping[str, str]:
+    PULP_FIXTURES_URL = pulp_cli_vars["PULP_FIXTURES_URL"]
+    result: t.MutableMapping[str, str] = {}
+    result.update(pulp_cli_vars)
+    
+    return result
