0fff829e9f6b8f02e97219d487d02230d811d12a
Yasen Trahnov <ytrahnov@redhat.com>
2025-03-25 18:45:34 +0100

    flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import typing as t
 import pytest
 
 pytest_plugins = "pytest_pulp_cli"
@@ -5,8 +6,6 @@ pytest_plugins = "pytest_pulp_cli"
 
 @pytest.fixture
 def pulp_cli_vars(pulp_cli_vars: t.MutableMapping[str, str]) -> t.MutableMapping[str, str]:
-    # If PULP_FIXTURES_URL is needed in the future, uncomment this assignment
-    # PULP_FIXTURES_URL = os.environ.get("PULP_FIXTURES_URL", "https://fixtures.pulpproject.org/")
     result: t.MutableMapping[str, str] = {}
     result.update(pulp_cli_vars)
 
